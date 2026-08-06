5c3caeb13b436c64b3e1f6ea2b0ccc77a7682951
intisar <intisar19961@hotmail.com>
2026-08-06 08:57:06 +0400

    up task 10

--- Task10/src/SumEvenNumbers.java
+++ Task10/src/SumEvenNumbers.java
@@ -1,21 +1,21 @@
 public class SumEvenNumbers {
 
     public static void main(String[] args){
-        System.out.println(printReverse(6));
+        System.out.println(SumEven(6));
 
     }
 
 
-    public static  int printReverse(int n){
+    public static  int SumEven(int n){
 
         // stop
         if(n == 0){
             return 0;
         }
         if(n % 2 == 0){
-            return n + printReverse(n - 1);
+            return n + SumEven(n - 1);
         }else {
-            return printReverse(n - 1);
+            return SumEven(n - 1);
         }
 
 
